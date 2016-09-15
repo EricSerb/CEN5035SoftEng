1f7a2d6c33177834c1a0196b2ffb8f37726f8a4c
Eric Serbousek <Eric Serbousek>
2016-09-15 11:50:54 -0400

    Added a never ending loader to the code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,26 @@
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by EricSerbousek on 9/15/2016.
  */
+
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
+        char[] loader = {'|', '\\', '-', '/', '|', '/', '-', '\\'};
         System.out.println("Hello Software Engineering World!");
+        System.out.print("Never ending loader...");
+        while(true){
+            int i = 0;
+            while(i < 4){
+                System.out.print(loader[i]);
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.print('\b');
+                ++i;
+            }
+        }
     }
 }
