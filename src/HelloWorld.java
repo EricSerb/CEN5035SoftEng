dbcde76827bef8476f5178ed98764fe5abc112ed
Eric Serbousek <Eric Serbousek>
2016-09-15 11:27:30 -0400

    Created a Hello World program in java to push to repo

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by EricSerbousek on 9/15/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello Software Engineering World!");
+    }
+}
